f05083f0ab1cfb3c54189573c3a7bc6a1b328799
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-05-23 12:27:23 -0600

    added functionality to make it easier to write data

--- label creation/generateCSV.py
+++ label creation/generateCSV.py
@@ -1,4 +1,54 @@
 import pandas as pd
 import numpy as np
+import re
+import csv
 
 df = pd.read_csv("C:\\Users\\abhin\\OneDrive\\Desktop\\Computing\\Nautical-Internship\\dataPreProcessing\\Kangraoo-App-Label-creation-Recommendation\\transcript_data\\data.csv",delimiter=",",encoding="utf-8")
+labels = ['In-person only', 'Remote only', 'Hybrid', 'No Preference', 'Full time', 'Part time', 'Internship', 'Acting / Performance', 'Analytics / Data', 'Building with hands', 'Design', 'Environment', 'Fashion', 'Food / Wine', 'Health / Wellness', 'Hospitality', 'Learning', 'Math', 'Meeting new people', 'Outdoors', 'Physical', 'Programming', 'Puzzles', 'Reading', 'Real Estate', 'Research', 'Service-oriented activities', 'Social media', 'Spending time with friends', 'Sports', 'Travel', 'Writing', 'Adaptable', 'Collaborative', 'Comfortable with ambiguity', 'Conflict management', 'Creative / Innovative', 'Critical thinking', 'Detail-oriented', 'Discreet', 'Emotionally intelligent', 'Friendly / Personable' ,'Interpersonal skills', 'Leadership', 'Multitasker', 'Organized', 'Persuasion', 'Problem-solving', 'Self-starter', 'Strong communicator', 'Works well under pressure', 'Accounting', 'Acting / Performings', 'Art / Design', 'Beauty', 'Film production', 'Finance', 'Home improvement', 'Management', 'Marketing', 'Multilingual', 'Photography', 'Presentation creation', 'Research', 'Sales', 'Social media', 'Teaching / Training', 'Tech-savvy', 'Verbal communication', 'Written communication']
+
+def getInput():
+    key = ""
+    usersDict = {}
+
+    while key != "stop":
+        print("Enter text:\n")
+        text = input()
+        key = text
+        if text != "stop":
+            labelList = list()
+            labelKey = ""
+            print("\n\nEnter Labels:\n")
+            while labelKey !=  "stop":
+                print("\nEnter Label: ")
+                label = input()
+                if labelKey !=  "stop":
+                    labelList.append(label)
+                labelKey = key
+            usersDict[text] = createLabels(labelList)
+        else:
+            print("\nStopped")
+    
+    writeToCSV(usersDict)
+
+def createLabels(inputLabel:list[str])->str:
+    newInput = [x.lower() for x in inputLabel]
+    binaryList = []
+    for label in labels:
+        if newInput.count(label.lower()) > 0:
+            binaryList.append(1)
+        else:
+            binaryList.append(0)
+    return binaryList
+
+def removeSpace(string:str)->str:
+    string = re.sub("[[]|[]]","",string)
+    return re.sub("(?<=,)\s","",string)
+
+def writeToCSV(userInfo:dict):
+    with open("C:\\Users\\abhin\\OneDrive\\Desktop\\Computing\\Nautical-Internship\\dataPreProcessing\\Kangraoo-App-Label-creation-Recommendation\\transcript_data\\data.csv",'w',newline='') as file:
+        for input in list(userInfo.keys()):
+            writer = csv.writer(file,delimiter=',')
+            writer.writerow([input]+userInfo[input])
+        file.close()
+
+getInput()
--- label creation/tags.py
+++ label creation/tags.py
@@ -20,26 +20,12 @@ import sys
     #        print(token.text)
 
 
-labels = ['In-person only', 'Remote only', 'Hybrid', 'No Preference', 'Full time', 'Part time', 'Internship', 'Acting / Performance', 'Analytics / Data', 'Building with hands', 'Design', 'Environment', 'Fashion', 'Food / Wine', 'Health / Wellness', 'Hospitality', 'Learning', 'Math', 'Meeting new people', 'Outdoors', 'Physical', 'Programming', 'Puzzles', 'Reading', 'Real Estate', 'Research', 'Service-oriented activities', 'Social media', 'Spending time with friends', 'Sports', 'Travel', 'Writing', 'Adaptable', 'Collaborative', 'Comfortable with ambiguity', 'Conflict management', 'Creative / Innovative', 'Critical thinking', 'Detail-oriented', 'Discreet', 'Emotionally intelligent', 'Friendly / Personable' ,'Interpersonal skills', 'Leadership', 'Multitasker', 'Organized', 'Persuasion', 'Problem-solving', 'Self-starter', 'Strong communicator', 'Works well under pressure', 'Accounting', 'Acting / Performings', 'Art / Design', 'Beauty', 'Film production', 'Finance', 'Home improvement', 'Management', 'Marketing', 'Multilingual', 'Photography', 'Presentation creation', 'Research', 'Sales', 'Social media', 'Teaching / Training', 'Tech-savvy', 'Verbal communication', 'Written communication']
 
 #import csv and then split into x and y
 
-def createLabels(inputLabel:list[str])->str:
-    newInput = [x.lower() for x in inputLabel]
-    binaryList = []
-    for label in labels:
-        if newInput.count(label.lower()) > 0:
-            binaryList.append(1)
-        else:
-            binaryList.append(0)
-    return removeSpace(str(binaryList))
 
-def removeSpace(string:str)->str:
-    string = re.sub("[[]|[]]","",string)
-    return re.sub("(?<=,)\s","",string)
 
 #remove stop words, tokenize, and then vectorize the data
-print(createLabels([sys.argv[2]]))
 
 
 #create the test, and train split
