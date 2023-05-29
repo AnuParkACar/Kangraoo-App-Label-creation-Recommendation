c5bd9a98e75d2a79a5826548cb1dc99193a221af
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-05-29 06:17:02 -0600

    Implemented two machine learning models

--- label creation/regressiveClassifierModel.py
+++ label creation/regressiveClassifierModel.py
@@ -4,6 +4,10 @@ import numpy as np
 import sklearn.feature_extraction.text as dataPreprocessing
 import sklearn.model_selection as modelSelection
 import neattext.functions as nfx
+from skmultilearn.problem_transform import ClassifierChain
+from sklearn.svm import SVC
+from sklearn.metrics import hamming_loss
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("C:\\Users\\abhin\\OneDrive\\Desktop\\Computing\\Nautical-Internship\\dataPreProcessing\\Kangraoo-App-Label-creation-Recommendation\\transcript_data\\data.csv",delimiter=",",encoding="utf-8")
 
@@ -15,13 +19,9 @@ features = featureTransformer.fit_transform(featureText).toarray()
 outputs = df[df.columns[1:]].values
 
 x_train,x_test,y_train,y_test = modelSelection.train_test_split(features,outputs,test_size=0.2,random_state=42)
-
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128,activation="relu"),
-    tf.keras.layers.Dense(128,activation="relu"),
-    tf.keras.layers.Dense(70,activation="sigmoid")
-])
-
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),loss="binary_crossentropy",metrics=["accuracy"])
-
-model.fit(x_train,y_train,batch_size=16,epochs=10)
+classifier = ClassifierChain(classifier=SVC())
+classifier.fit(X=x_train,y=y_train)
+prediction = classifier.predict(x_test)
+print(prediction.toarray())
+print("\nAccuracy: " + str(accuracy_score(y_test,prediction.toarray())))
+print("\nLoss: " + str(hamming_loss(y_test,prediction.toarray())))
