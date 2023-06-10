69ba92b5fbb25a3b3c3c7fbc985537a31a8ab6f2
Abhinav parbhakar <abhinav.parbhakar302@gmail.com>
2023-06-09 20:50:08 -0600

    Converted classfier model

--- label creation/regressiveClassifierModel.py
+++ label creation/regressiveClassifierModel.py
@@ -6,43 +6,37 @@ import sklearn.model_selection as modelSelection
 import neattext.functions as nfx
 from skmultilearn.problem_transform import ClassifierChain
 from sklearn.svm import SVC
-from sklearn.metrics import hamming_loss
-from sklearn.metrics import accuracy_score
 from generateCSV import generateLabels
 
-df = pd.read_csv("C:\\Users\\abhin\\OneDrive\\Desktop\\Computing\\Nautical-Internship\\dataPreProcessing\\Kangraoo-App-Label-creation-Recommendation\\transcript_data\\data.csv",delimiter=",",encoding="utf-8")
+class Classifier:
+    def __init__(self) -> None:
+    
+        df = pd.read_csv("C:\\Users\\abhin\\OneDrive\\Desktop\\Computing\\Nautical-Internship\\dataPreProcessing\\Kangraoo-App-Label-creation-Recommendation\\transcript_data\\data.csv",delimiter=",",encoding="utf-8")
 
-featureText = df["Transcriptions"].apply(nfx.remove_stopwords,nfx.remove_non_ascii)
-
-
-featureTransformer = dataPreprocessing.TfidfVectorizer()
-fittedVectorizer = featureTransformer.fit(featureText)
-#features = featureTransformer.fit_transform(featureText).toarray()
-features = fittedVectorizer.transform(featureText).toarray()
-outputs = df[df.columns[1:].tolist()].values
+        featureText = df["Transcriptions"].apply(nfx.remove_stopwords,nfx.remove_non_ascii)
 
 
-x_train,x_test,y_train,y_test = modelSelection.train_test_split(features,outputs,test_size=0.2,)
-
-for i in range(y_train.shape[1]):
-    unique_values = np.unique(y_train[:, i])
-    if len(unique_values) < 2:
-        print(f"Output column {i} has less than two unique values: {unique_values}")
-
-
-print("\n\nLabel: {}".format(df.columns[51]))
-
-classifier = ClassifierChain(classifier=SVC())
-classifier.fit(X=x_train,y=y_train)
-prediction = classifier.predict(x_test)
-print(prediction.toarray())
-print("\nAccuracy: " + str(accuracy_score(y_test,prediction.toarray())))
-print("\nLoss: " + str(hamming_loss(y_test,prediction.toarray())))
+        featureTransformer = dataPreprocessing.TfidfVectorizer()
+        self.fittedVectorizer = featureTransformer.fit(featureText)
+        #features = featureTransformer.fit_transform(featureText).toarray()
+        features = self.fittedVectorizer.transform(featureText).toarray()
+        outputs = df[df.columns[1:].tolist()].values
 
 
-textVector = fittedVectorizer.transform(["Hi, I'm Danielle, the HR manager at Nebula Fashion House. We're looking for a passionate and creative fashion designer to join our innovative team. This is a full-time, in-person role based in our vibrant New York studio. The ideal candidate should have an eye for aesthetics, be comfortable with ambiguity as trends are ever-changing, and should have excellent interpersonal skills to effectively communicate with our team and clients. A background in Art and Design will be highly advantageous for this role"]).toarray()
+        x_train,x_test,y_train,y_test = modelSelection.train_test_split(features,outputs,test_size=0.2)
 
-labels = generateLabels()
-predictedLabels = classifier.predict(textVector).toarray()
-ok = predictedLabels[0].tolist()
-print(labels.generateListOfLabels(ok))
+
+        self.classifier = ClassifierChain(classifier=SVC())
+        self.classifier.fit(X=features,y=outputs)
+
+
+    def predict(self,transcript:str)->str:
+        textVector = self.fittedVectorizer.transform([transcript]).toarray()
+
+        labels = generateLabels()
+        predictedLabels = self.classifier.predict(textVector).toarray()
+        ok = predictedLabels[0].tolist()
+        return str(labels.generateListOfLabels(ok))
+    
+cl = Classifier()
+print(cl.predict("Hi, I'm Danielle, the HR manager at Nebula Fashion House. We're looking for a passionate and creative fashion designer to join our innovative team. This is a full-time, in-person role based in our vibrant New York studio. The ideal candidate should have an eye for aesthetics, be comfortable with ambiguity as trends are ever-changing, and should have excellent interpersonal skills to effectively communicate with our team and clients. A background in Art and Design will be highly advantageous for this role"))
